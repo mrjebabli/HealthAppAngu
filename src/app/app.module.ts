399ceb326f44c3e65fc769c1cf763b2fa10ad02f
mohamed riadh jebabli <26mohamed.jebabli@gmail.com>
2021-05-09 13:32:56 +0100

    add doctors (component)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DoctorsComponent } from './doctors/doctors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoctorsComponent
   ],
   imports: [
     BrowserModule
